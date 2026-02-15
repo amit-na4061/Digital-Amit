9e6b825c2233f817162d5f87d6c7445f714610cf
Amit Nagaich <153860840+amit-na4061@users.noreply.github.com>
2026-02-15 21:16:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ Using Qdrant and Google Gemini API
 
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import ConversationalRetrievalChain
+from langchain_community.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain_core.prompts import PromptTemplate
 from rag_pipeline import AmitChatbotRAG
